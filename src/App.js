ed1ea3a50e589329a8696eb9e916baae249b3508
Alex Bailey <baileymk@hotmail.com>
2021-11-19 15:11:41 +0000

    refactored code and now prevents multiple operator inputs

--- src/App.js
+++ src/App.js
@@ -1,67 +1,65 @@
 import React from "react"
 import { useState } from "react"
-import { evaluate, sqrt, square  } from "mathjs"
-// const math = create(all, {})
+import { evaluate, sqrt, square } from "mathjs"
+
 
 const App = () => {
   const [buttons] = useState(["square", "sqrt", "clear", "back", 7, 8, 9, "*", 4, 5, 6, "-", 1, 2, 3, "+", ".", 0, "=", "/"])
-  const [output, setOutput] = useState([])
+  const ops = ["/", "*", "+", "-", "."]
+  const [output, setOutput] = useState([]) //JUST USE A STRING INSTEAD OF AN ARRAY NEXT TIME FFS
   const [sum, setSum] = useState([])
 
-  const clickHandler = (index, e) => {
-    
-    if (e.target.innerHTML === "=" && output) {
-      equalsHandler() }
-    else if ( e.target.innerHTML === "square" ){
-      squareHandler()
-    } else if (e.target.innerHTML === "sqrt"){
-      sqrtHandler()
-    } else if ( e.target.innerHTML === "clear"){
-      screenClear()
-    } else if (e.target.innerHTML === "back"){
-      const storedOutput = [...output]
-      storedOutput.pop()
-      setOutput(storedOutput)
+  const clickHandler = (index, e, button) => {
+    ops.includes(button) && ops.includes(output[output.length - 1]) ?
+      console.log("don't do that")
+      : e.target.innerHTML === "=" && output ?
+        equalsHandler()
+        : e.target.innerHTML === "square" ?
+          squareHandler()
+          : e.target.innerHTML === "sqrt" ?
+            sqrtHandler()
+            : e.target.innerHTML === "clear" ?
+              screenClear()
+              : e.target.innerHTML === "back" ?
+                backHandler()
+                : outputHandler(index)
 
-      const storedSum = [...sum]
-      storedSum.pop()
-      setSum(storedSum)
-    } else if (output === "error") {
-      setOutput("error")
-    }
-    else {
-      const storedOutput = [...output]
-      storedOutput.push(buttons[index])
-      setOutput(storedOutput)
+  }
 
-      const storedSum = [...sum]
-      storedSum.push(buttons[index])
-      setSum(storedSum)
-    }
+  const outputHandler = (index) => {
+    const storedOutput = [...output]
+    storedOutput.push(buttons[index])
+    setOutput(storedOutput)
+
+    const storedSum = [...sum]
+    storedSum.push(buttons[index])
+    setSum(storedSum)
+  }
+
+  const backHandler = () => {
+    const storedOutput = [...output]
+    storedOutput.pop()
+    setOutput(storedOutput)
+
+    const storedSum = [...sum]
+    storedSum.pop()
+    setSum(storedSum)
   }
   const squareHandler = () => {
-    if (output.length >0) {
+    if (output.length > 0) {
       const storedOutput = [...output]
-      const joinedOutput = storedOutput.join("")
-      const answer = square(joinedOutput)
-      const strans = answer.toString()
-      const splitans = strans.split("")
-      setOutput(splitans)
+      setOutput(square(storedOutput.join("")).toString().split(""))
 
       const storedSum = [...sum]
       storedSum.push("²")
       setSum(storedSum)
     }
-    
+
   }
   const sqrtHandler = () => {
-    if (output.length >0) {
+    if (output.length > 0) {
       const storedOutput = [...output]
-      const joinedOutput = storedOutput.join("")
-      const answer = sqrt(joinedOutput).toFixed(2)
-      const strans = answer.toString()
-      const splitans = strans.split("")
-      setOutput(splitans)
+      setOutput(sqrt(storedOutput.join("")).toFixed(2).toString().split(""))
 
       const storedSum = [...sum]
       storedSum.unshift("√")
@@ -72,15 +70,11 @@ const App = () => {
     setOutput("")
     setSum("")
   }
-  
+
   const equalsHandler = () => {
     if (output.length > 1) {
       const storedOutput = [...output]
-      const joinedOutput = storedOutput.join("")
-      const answer = evaluate(joinedOutput)
-      const strans = answer.toString()
-      const splitans = strans.split("")
-      setOutput(splitans)
+      setOutput(evaluate(storedOutput.join("")).toString().split(""))
 
       const storedSum = [...sum]
       storedSum.push(")")
@@ -92,14 +86,14 @@ const App = () => {
   return (
     <div className="calculator">
       <div className="screen">{output}<div className="sum">{sum}</div></div>
-      
+
       <div className="container">
 
         {buttons.map((button, index) => {
-          return <button className="btn" key={index} onClick={(e) => clickHandler(index, e)}>{button}</button>
+          return <button className="btn" key={index} onClick={(e) => clickHandler(index, e, button)}>{button}</button>
 
         })}
-        
+
       </div>
     </div>
   )
